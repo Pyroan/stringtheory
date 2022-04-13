8dc07ad0be47b23d662d4a8f61d5b65cd1edb8b1
Violet Moore <Pyroan@users.noreply.github.com>
2022-04-13 12:58:06 -0700

    add option to isolate steps

--- globals.lua
+++ globals.lua
@@ -1,9 +1,12 @@
-constants = {
+globals = {
     hoopRadius = 260,           -- inner radius of the circle defined by the nails
-    hoopResolution = 13,        -- number of nails
-    nailWidth = 15,            -- radius of a nail body.
+    hoopResolution = 10,        -- number of nails
+    nailWidth = 0,            -- radius of a nail body.
     evaluatorResolution = 500,  -- side length of grid used by evaluator function
     stringWidth = 0.05,         -- diameter of the string
+
+    doIsolateStep = false,
+    isolateStep = 0,
 
     iterationsPerTemp = 100, -- number of new states to check before decreasing the temperature
     initialTemp = 100
--- main.lua
+++ main.lua
@@ -1,12 +1,14 @@
 local nuklear = require "nuklear"
-require "constants"
+require "globals"
 require "hoop"
+require "util"
 
 local ui
 function love.load()
     love.window.setMode(1280, 720)
+    love.window.setTitle("Vi's String Theory")
     ui = nuklear.newUI()
-    initHoop(constants['hoopResolution'], constants['hoopRadius'], constants['nailWidth'])
+    initHoop(globals['hoopResolution'], globals['hoopRadius'], globals['nailWidth'])
 end
 
 angle = 0
@@ -14,68 +16,78 @@ function love.update(delta)
     ui:frameBegin()
     if ui:windowBegin('Settings', 0, 0, 180, love.graphics.getHeight(), 'border', 'movable', 'minimizable', 'title') then
         ui:layoutRow('dynamic', 35, 1)
-        ui:label("Nail Radius: "..constants['nailWidth'])
-        constants['nailWidth'] = ui:slider(0, constants['nailWidth'], 100, 1)
+        ui:label("Nail Radius: " .. globals['nailWidth'])
+        globals['nailWidth'] = ui:slider(0, globals['nailWidth'], 100, 1)
 
-        ui:label("Hoop Radius: "..constants['hoopRadius'])
-        constants['hoopRadius'] = ui:slider(0, constants['hoopRadius'], love.graphics.getHeight() / 2, 1)
-        constants['hoopResolution'] = ui:property('Nails', 2, constants['hoopResolution'], 128, 1, 1)
-        local c = constants['hoopResolution']
-        c = (constants['nailWidth'] > 0 and 4 or 1) * (c * (c-1)) / 2
-        ui:label("Expected Strings: ".. c)
-        ui:label("Actual Strings: ".. strings, 'wrap', strings == c and '#FFFFFF' or '#FF0000')
+        ui:label("Hoop Radius: " .. globals['hoopRadius'])
+        globals['hoopRadius'] = ui:slider(0, globals['hoopRadius'], love.graphics.getHeight() / 2, 1)
+        globals['hoopResolution'] = ui:property('Nails', 2, globals['hoopResolution'], 128, 1, 1)
+        globals['doIsolateStep'] = ui:checkbox("Isolate Step", globals['doIsolateStep'])
+        if globals['doIsolateStep'] then
+            globals['isolateStep'] = ui:property("Step", 0, globals['isolateStep'], math.floor(globals['hoopResolution'] / 2) - 1, 1, 1)
+        end
+        local c = globals['hoopResolution']
+        if globals['doIsolateStep'] then
+            c = globals['isolateStep']+1 ~= #nails/2 and #nails or #nails/2
+            ui:label("Cycles: ".. gcd(#nails, globals['isolateStep']+1))
+        else
+            c = (globals['nailWidth'] > 0 and 4 or 1) * (c * (c - 1)) / 2
+        end
+        ui:label("Expected Strings: " .. c)
+        ui:label("Actual Strings: " .. strings, 'wrap', strings == c and '#FFFFFF' or '#FF0000')
     end
     ui:windowEnd()
     ui:frameEnd()
-    angle = angle + ((1/60) * delta * math.pi)
-    if angle > 2 * math.pi then angle = angle - 2 * math.pi end
-    initHoop(constants['hoopResolution'], constants['hoopRadius'], constants['nailWidth'], angle)
+    angle = angle + ((1 / 60) * delta * math.pi)
+    if angle > 2 * math.pi then
+        angle = angle - 2 * math.pi
+    end
+    initHoop(globals['hoopResolution'], globals['hoopRadius'], globals['nailWidth'], angle)
 end
 
 function love.draw()
-    drawHoop(constants['nailWidth'])
+    drawHoop(globals['nailWidth'])
     ui:draw()
 end
 
-
 function love.keypressed(key, scancode, isrepeat)
-	if ui:keypressed(key, scancode, isrepeat) then
-		return -- event consumed
-	end
+    if ui:keypressed(key, scancode, isrepeat) then
+        return -- event consumed
+    end
 end
 
 function love.keyreleased(key, scancode)
-	if ui:keyreleased(key, scancode) then
-		return -- event consumed
-	end
+    if ui:keyreleased(key, scancode) then
+        return -- event consumed
+    end
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
-	if ui:mousepressed(x, y, button, istouch, presses) then
-		return -- event consumed
-	end
+    if ui:mousepressed(x, y, button, istouch, presses) then
+        return -- event consumed
+    end
 end
 
 function love.mousereleased(x, y, button, istouch, presses)
-	if ui:mousereleased(x, y, button, istouch, presses) then
-		return -- event consumed
-	end
+    if ui:mousereleased(x, y, button, istouch, presses) then
+        return -- event consumed
+    end
 end
 
 function love.mousemoved(x, y, dx, dy, istouch)
-	if ui:mousemoved(x, y, dx, dy, istouch) then
-		return -- event consumed
-	end
+    if ui:mousemoved(x, y, dx, dy, istouch) then
+        return -- event consumed
+    end
 end
 
 function love.textinput(text)
-	if ui:textinput(text) then
-		return -- event consumed
-	end
+    if ui:textinput(text) then
+        return -- event consumed
+    end
 end
 
 function love.wheelmoved(x, y)
-	if ui:wheelmoved(x, y) then
-		return -- event consumed
-	end
+    if ui:wheelmoved(x, y) then
+        return -- event consumed
+    end
 end
