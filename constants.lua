758db868660398ef09fa54d8f3b59e9b66204f32
Violet Moore <Pyroan@users.noreply.github.com>
2022-04-13 10:52:13 -0700

    add ui sliders

--- constants.lua
+++ constants.lua
@@ -1,7 +1,7 @@
 constants = {
     hoopRadius = 260,           -- inner radius of the circle defined by the nails
     hoopResolution = 13,        -- number of nails
-    nailWidth = 10,            -- radius of a nail body.
+    nailWidth = 15,            -- radius of a nail body.
     evaluatorResolution = 500,  -- side length of grid used by evaluator function
     stringWidth = 0.05,         -- diameter of the string
 
--- main.lua
+++ main.lua
@@ -1,17 +1,79 @@
-require("constants")
-require("hoop")
+local nuklear = require "nuklear"
+require "constants"
+require "hoop"
 
+local ui
 function love.load()
+    love.window.setMode(1280, 720)
+    ui = nuklear.newUI()
     initHoop(constants['hoopResolution'], constants['hoopRadius'], constants['nailWidth'])
 end
 
 angle = 0
 function love.update(delta)
-    angle = angle + (0.25 * delta * math.pi)
+    ui:frameBegin()
+    if ui:windowBegin('Settings', 0, 0, 180, love.graphics.getHeight(), 'border', 'movable', 'minimizable', 'title') then
+        ui:layoutRow('dynamic', 35, 1)
+        ui:label("Nail Radius: "..constants['nailWidth'])
+        constants['nailWidth'] = ui:slider(0, constants['nailWidth'], 100, 1)
+
+        ui:label("Hoop Radius: "..constants['hoopRadius'])
+        constants['hoopRadius'] = ui:slider(0, constants['hoopRadius'], love.graphics.getHeight() / 2, 1)
+
+        ui:label("Hoop Resolution: "..constants['hoopResolution'])
+        constants['hoopResolution'] = ui:slider(2, constants['hoopResolution'], 128,1)
+    end
+    ui:windowEnd()
+    ui:frameEnd()
+    angle = angle + ((1/60) * delta * math.pi)
     if angle > 2 * math.pi then angle = angle - 2 * math.pi end
     initHoop(constants['hoopResolution'], constants['hoopRadius'], constants['nailWidth'], angle)
 end
 
 function love.draw()
     drawHoop(constants['nailWidth'])
+    ui:draw()
+end
+
+
+function love.keypressed(key, scancode, isrepeat)
+	if ui:keypressed(key, scancode, isrepeat) then
+		return -- event consumed
+	end
+end
+
+function love.keyreleased(key, scancode)
+	if ui:keyreleased(key, scancode) then
+		return -- event consumed
+	end
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+	if ui:mousepressed(x, y, button, istouch, presses) then
+		return -- event consumed
+	end
+end
+
+function love.mousereleased(x, y, button, istouch, presses)
+	if ui:mousereleased(x, y, button, istouch, presses) then
+		return -- event consumed
+	end
+end
+
+function love.mousemoved(x, y, dx, dy, istouch)
+	if ui:mousemoved(x, y, dx, dy, istouch) then
+		return -- event consumed
+	end
+end
+
+function love.textinput(text)
+	if ui:textinput(text) then
+		return -- event consumed
+	end
+end
+
+function love.wheelmoved(x, y)
+	if ui:wheelmoved(x, y) then
+		return -- event consumed
+	end
 end
