16c379d85b362392381c797e8540089db1c8faf0
Violet Moore <Pyroan@users.noreply.github.com>
2022-04-15 18:38:22 -0700

    more refactoring...

--- hoop.lua
+++ hoop.lua
@@ -1,6 +1,8 @@
-require "ztring"
+require "stringstate"
 require "util"
+require "ztring"
 
+stringCount = 0
 --- Initialize the "hoop" - the polygon of nails and strings that we're drawing.
 ---@param nailResolution integer
 ---@param hoopRadius integer
@@ -9,14 +11,14 @@ require "util"
 function initHoop(nailResolution, hoopRadius, nailRadius, angle)
     angle = angle or 0
     nails = initNails(nailResolution, hoopRadius, nailRadius, angle)
-    strings = initStrings()
+    stringState = StringState:new({}, nails)
 end
 
 --- Draw the hoop
 ---@param nailRadius integer
 function drawHoop(nailRadius)
     drawNails(nailRadius)
-    drawStrings(nailRadius)
+    stringCount = stringState:draw(nailRadius)
 end
 
 function initNails(resolution, hoopRadius, nailRadius, angle)
@@ -43,41 +45,3 @@ function drawNails(nailRadius)
     end
 end
 
-stringCount = 0
-
-function initStrings()
-    local strings = {}
-    for i = 1, #nails do
-        for j = i + 1, #nails do
-            for k = 1, 4 do
-                local s = String:new({}, nails[i], nails[j], k)
-                strings[#strings + 1] = s
-            end
-        end
-    end
-    return strings
-end
-
-function drawStrings(nailRadius)
-    stringCount = 0
-    love.graphics.setColor(HSL(angle / (2 * math.pi), 1, 0.5, 1))
-    for s = 1, #strings do
-        if not globals['doIsolateStep'] or getStringStep(strings[s]) == globals['isolateStep'] then
-            -- love.graphics.setColor(HSL(strings[s].type/5 -0.2, 1, 0.5, 1))
-            if strings[s]:draw(nailRadius) then
-                stringCount = stringCount + 1
-            end
-        end
-    end
-    love.graphics.setColor(1, 1, 1, 1)
-end
-
---- return the minimum number of nails needed to get from `string`s source node to its destination node
----@param string table
-function getStringStep(string)
-    local s = string.destNode.id - string.sourceNode.id
-    if s > #nails / 2 then
-        s = string.sourceNode.id + #nails - string.destNode.id
-    end
-    return s
-end
